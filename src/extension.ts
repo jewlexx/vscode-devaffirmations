dcae61fe36d5a96034b92f3d0e8e9f978e37aa37
jamesinaxx <grassojames5@gmail.com>
2021-04-29 12:50:07 +1000

    Updated log message

--- src/extension.ts
+++ src/extension.ts
@@ -8,9 +8,7 @@ import axios from 'axios';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log(
-		'Congratulations, your extension "vscode-devaffirmations" is now active!'
-	);
+	console.log('Extension vscode-devaffirmations has been enabled');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
