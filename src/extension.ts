4d48961acbcdd5643b8208fd840e8305babbde34
jamesinaxx <grassojames5@gmail.com>
2021-05-25 16:40:36 +1000

    Fixed major bug with not showing affirmations

--- src/extension.ts
+++ src/extension.ts
@@ -5,19 +5,19 @@ import { getRandomAffirmation } from './lib/affirmations';
 import axios from 'axios';
 
 async function getAffirmation() {
-	try {
-		vscode.window.showInformationMessage(
-			(
-				await axios.get('https://jamesinaxx.me/api/affirmations', {
-					timeout: 10,
-				})
-			).data.affirmation
-		);
-	} catch (error) {
-		if (error.dueToNoInternetConnection) {
-			vscode.window.showInformationMessage(getRandomAffirmation());
-		}
-	}
+	const affirmation = new Promise((resolve, _reject) => {
+		axios
+			.get('https://jamesinaxx.me/api/affirmations', {
+				timeout: 10,
+			})
+			.then(async res => resolve(res.data.affirmation))
+			.catch(async () => resolve(await getRandomAffirmation()));
+	});
+
+	affirmation.then((res: any) => {
+		console.log('Showing affirmation: ' + res);
+		vscode.window.showInformationMessage(res);
+	});
 }
 
 // this method is called when your extension is activated
@@ -32,12 +32,12 @@ export async function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand(
+	let command = vscode.commands.registerCommand(
 		'vscode-devaffirmations.affirm',
 		() => getAffirmation()
 	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(command);
 }
 
 // this method is called when your extension is deactivated
